b51cc06f8dfac9a6b1eb4b13f2204c4a07d5446f
nethma-dewmini <rathnayakarmnd.22@uom.lk>
2026-03-20 15:48:40 +0530

    merged sms registerd with security deposit

--- src/pages/General.tsx
+++ src/pages/General.tsx
@@ -1,5 +1,6 @@
 import { useState, useEffect } from "react";
 import ActiveCustomersSalesByTariff from "../mainTopics/general/ActiveCustomersSalesByTariff";
+import SecurityDepositContractDemandBulk from "../mainTopics/general/Securitydepositcontractdemandbulk";
 import RegisteredConsumersForSMSAlerts from "../mainTopics/general/RegisteredConsumersForSMSAlerts";
 import { data as sidebarData } from "../data/SideBarData";
 import SubtopicCard from "../components/shared/SubtopicCard";
@@ -14,7 +15,6 @@ const General = () => {
   const [expandedCard, setExpandedCard] = useState<number | null>(null);
 
   useEffect(() => {
-    // Get General topic's subtopics directly from sidebarData
     const generalTopic = sidebarData.find((topic) => topic.name === "General");
     if (generalTopic) {
       setSubtopics(generalTopic.subtopics);
@@ -22,17 +22,15 @@ const General = () => {
   }, []);
 
   const toggleCard = (id: number) => {
-    if (expandedCard === id) {
-      setExpandedCard(null);
-    } else {
-      setExpandedCard(id);
-    }
+    setExpandedCard((prev) => (prev === id ? null : id));
   };
 
   const renderSubtopicContent = (subtopicName: string) => {
     switch (subtopicName) {
       case "Active customers and sales by tariff":
         return <ActiveCustomersSalesByTariff />;
+      case "Security deposit & Contract Demand - Bulk":
+        return <SecurityDepositContractDemandBulk />;
       case "Registered consumers for SMS alerts":
         return <RegisteredConsumersForSMSAlerts />;
       case "Bill calculation":
@@ -40,79 +38,40 @@ const General = () => {
       case "List of government accounts":
       case "Largest 100 customer details":
       case "Sequence change accounts":
-
       case "Retails Journal":
-
       case "Arrears position – meter reader wise":
-
       case "List of customers (enlisted in Master Invoices)":
-
       case "Disconnection list":
-
       case "Shakthi LED distribution summary":
-
       case "Standing order report":
-
       case "Finalized Accounts":
-
       case "Outstanding Dues":
-
       case "Largest Consumption":
-
-      case "Security deposit & Contract Demand - Bulk":  
-
         return <div>{subtopicName} Content</div>;
-
       default:
-
         return (
-
           <div className="text-red-500 text-xs">
-
             No content available for {subtopicName}
-
           </div>
-
         );
-
     }
-
   };
 
-
-
   return (
-
     <div className="flex flex-col gap-4 pt-5">
-
       {subtopics.map((subtopic) => (
-
         <SubtopicCard
-
           key={subtopic.id}
-
           id={subtopic.id}
-
           title={subtopic.name}
-
           expanded={expandedCard === subtopic.id}
-
           onToggle={toggleCard}
-
         >
-
           {renderSubtopicContent(subtopic.name)}
-
         </SubtopicCard>
-
       ))}
-
     </div>
-
   );
-
 };
-
-
 
 export default General;
