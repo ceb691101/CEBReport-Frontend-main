bf6a450d126831ba19a880e247d0a2f344426183
Nethmini-jayasekara <132090104+Nethmini-jayasekara@users.noreply.github.com>
2026-03-24 15:37:53 +0530

    UPDATE

--- src/components/login/LoginCard.tsx
+++ src/components/login/LoginCard.tsx
@@ -52,7 +52,7 @@ const LoginCard = () => {
 
       if (IsLogged?.Logged) {
         toast.success("Login successful!", { autoClose: 2000 });
-        navigate("/adminhome");
+        navigate("/home");
 
         const userData = await postJSON("/CBRSAPI/CBRSEPFNOLogin", {
           Username: username,
