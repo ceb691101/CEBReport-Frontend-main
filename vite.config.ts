8017022904e37442168bc60d29587cb9e7ee057a
Madara0421D <madaradushyanthi@gmail.com>
2026-06-10 11:08:16 +0530

    restore vite.config

--- vite.config.ts
+++ vite.config.ts
@@ -1,4 +1,4 @@
-import { defineConfig } from "vite";
+import {defineConfig} from "vite";
 import react from "@vitejs/plugin-react";
 import tailwindcss from "@tailwindcss/vite";
 
@@ -22,35 +22,6 @@ export default defineConfig({
 				changeOrigin: true,
 				secure: false,
 			},
-<<<<<<< HEAD
-			// Local testing: route CC Application report to local .NET backend
-			"/misapi/api/solarjobs/ccapplication": {
-=======
-			"/misapi/api/phv-obsolete-idle-fifo": {
->>>>>>> c998d1df1da9fd2e403ae3fac07d2e5814e3d305
-				target: "http://localhost:44381",
-				changeOrigin: true,
-				secure: false,
-				rewrite: (path) => path.replace(/^\/misapi/, ""),
-			},
-<<<<<<< HEAD
-=======
-			"/misapi/api/phv-damage-fifo": {
-				target: "http://localhost:44381",
-				changeOrigin: true,
-				secure: false,
-				rewrite: (path) => path.replace(/^\/misapi/, ""),
-			},
-
-			"/misapi/api/areatrialbalance": {
-				target: "http://localhost:44381",
-				changeOrigin: true,
-				secure: false,
-				rewrite: (path) => path.replace(/^\/misapi/, ""),
-			},
-
-
->>>>>>> c998d1df1da9fd2e403ae3fac07d2e5814e3d305
 			"/misapi": {
 				target: "http://10.128.1.126",
 				changeOrigin: true,
@@ -142,14 +113,11 @@ export default defineConfig({
 				target: "http://smartceb.ceb:81",
 				changeOrigin: true,
 				secure: false,
-<<<<<<< HEAD
-=======
 			},	
 			"/MRMSAPI": {
 				target: "http://itservice.cebinfo.ceb",
 				changeOrigin: true,
 				secure: false,
->>>>>>> c998d1df1da9fd2e403ae3fac07d2e5814e3d305
 			},
 			"/misreportsapi": {
 				target: "http://localhost:44381",
@@ -163,8 +131,6 @@ export default defineConfig({
 				secure: false,
 				rewrite: (path) => path.replace(/^\/pivapi/, ""),
 			},
-
-
 		},
 	},
-});	
+});
